a876013b4d61b559b89a7266821678f82c280789
Shiner KOMISSA ZOTSU <essikomissa@gmail.com>
2022-11-25 13:44:36 +0000

    Update footer.blade.php
    Update du footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2021 <a href="">CLS BETHESDA</a>.</strong>
-    All rights reserved.
+    <strong>Copyright &copy; 2022 <a href="">CLS BETHESDA</a>.</strong>
+    Tous droits réservés.
     <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.2.0
+      <b>Version</b> 1.0
     </div>
   </footer>
